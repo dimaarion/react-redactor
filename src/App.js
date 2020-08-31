6c3d9c8703998365697106a950482a5fcc3ee4ae
dimaarion <dimaarion@gmail.com>
2020-08-31 18:04:30 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect} from "react";
-import Sizeplus from '../Sizeplus'
+import Sizeplus from '../Sizeplus';
+import Sizeminus from '../Sizeminus';
 import {
   CompositeDecorator,
   ContentState,
@@ -31,41 +32,6 @@ function App() {
   function fonts(a = []) {
     return a;
   }
-
-  
-
-  let sizesminus = (
-    <svg
-      width="1em"
-      height="1em"
-      viewBox="0 0 16 16"
-      className="bi bi-arrows-move"
-      fill="currentColor"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        fillRule="evenodd"
-        d="M6.5 8a.5.5 0 0 0-.5-.5H1.5a.5.5 0 0 0 0 1H6a.5.5 0 0 0 .5-.5z"
-      />
-      <path
-        fillRule="evenodd"
-        d="M3.854 5.646a.5.5 0 0 0-.708 0l-2 2a.5.5 0 0 0 0 .708l2 2a.5.5 0 0 0 .708-.708L2.207 8l1.647-1.646a.5.5 0 0 0 0-.708zM9.5 8a.5.5 0 0 1 .5-.5h4.5a.5.5 0 0 1 0 1H10a.5.5 0 0 1-.5-.5z"
-      />
-      <path
-        fillRule="evenodd"
-        d="M12.146 5.646a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L13.793 8l-1.647-1.646a.5.5 0 0 1 0-.708zM8 9.5a.5.5 0 0 0-.5.5v4.5a.5.5 0 0 0 1 0V10a.5.5 0 0 0-.5-.5z"
-      />
-      <path
-        fillRule="evenodd"
-        d="M5.646 12.146a.5.5 0 0 0 0 .708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8 13.793l-1.646-1.647a.5.5 0 0 0-.708 0zM8 6.5a.5.5 0 0 1-.5-.5V1.5a.5.5 0 0 1 1 0V6a.5.5 0 0 1-.5.5z"
-      />
-      <path
-        fillRule="evenodd"
-        d="M5.646 3.854a.5.5 0 0 1 0-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8 2.207 6.354 3.854a.5.5 0 0 1-.708 0z"
-      />
-    </svg>
-  );
-
   function interat(sizesplus, sizesminus, bol) {
     return bol === true ? sizesplus : sizesminus;
   }
@@ -258,7 +224,7 @@ const contentStateWithEntity = contentState.createEntity('LINK', 'MUTABLE', {
           className="sizes"
           onClick={() => setsizes(sizes === true ? false : true)}
         >
-       <div> {sizes === true? <Sizeplus/>:''}</div>  
+       <div> {sizes === true? <Sizeplus/>:<Sizeminus/>}</div>  
         </div>
       </div>
       
