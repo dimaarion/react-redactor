ceb6d5493a7b691a6252ff8d89fd07f9c77e52d0
dimaarion <dimaarion@gmail.com>
2020-09-18 13:36:55 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
           className="text_block"
           contentEditable="true"
           onKeyPress={e => {
-            e.key ? console.log(e.keyCode) : "";
+            e.key ? console.log(e.key) : "";
             
           }}
         />
