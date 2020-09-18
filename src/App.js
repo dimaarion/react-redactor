1206ec5ba7ae34c1e85b58974be82685cf094aca
dimaarion <dimaarion@gmail.com>
2020-09-19 01:18:48 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import "./bootstrap.css";
 function App() {
   const [sizes, setsizes] = useState(true);
   const [textcont, settextcont] = useState(false);
-  const [textbl, settextbl] = useState("");
+  const [textbl, settextbl] = useState("div");
   const [headerT, setheaderT] = useState("unstyled***");
   const [headerType, setheaderType] = useState("div");
   const [items, setitems] = useState(0);
@@ -17,10 +17,9 @@ function App() {
     return bol === true ? sizesplus : sizesminus;
   }
   function test(e) {
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * Math.floor(max));
-    }
+   
     let text_block = document.querySelector(".text_block");
+    /*
     let d = Object.values(text_block.getElementsByTagName("div"));
     let f = 1;
     f = f + 1;
@@ -28,7 +27,18 @@ function App() {
     let y = d.map((el, i) => (el.className = "item-" + (i + 1)));
     let c = Object.values(text_block.getElementsByClassName("item-" + items));
     console.log(c)
-    d.map((el, i) => (el.onclick = els => setitems(i + 1)));
+    d.map((el, i) => (el.onclick = els => setitems(i + 1)));*/
+
+    let a = ['div', 'h1', 'p'];
+    let y = a.filter((x)=> Object.values(text_block.getElementsByTagName(x)).length > 0)
+    .map((x)=>Object.values(text_block.getElementsByTagName(x)));
+     let gh = y[0];
+     if(gh !== undefined){
+      
+       let ghj = gh.map((x,i)=>x.onclick = x => setitems(i + 1))
+      gh.map((el, i) => (el.className = "item-" + (i + 1)));
+     }
+     
   }
    
   useEffect(() => {}, [items]);
@@ -40,9 +50,10 @@ function App() {
           <Panel setheaderType={setheaderType} />
           <div
             onClick={() => {
+              settextbl('h1')
               let text_block = document.querySelector(".text_block");
               let d = Object.values(text_block.getElementsByClassName("item-" + items));
-           ( d[0] !== undefined)? d[0].outerHTML = '<h1 class = "item-'+ items +'" >' + d[0].innerText + '</h1>':''
+           ( d[0] !== undefined)? d[0].outerHTML = '<' + textbl + ' class = "item-'+ items + '" >' + d[0].innerText + '</'+textbl +'>':''
            console.log(d)
             }}
           >
