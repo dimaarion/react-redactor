d8a513b47d48497633be927899e9365bd5aa2365
dimaarion <dimaarion@gmail.com>
2020-10-17 07:07:11 +0300

    1 file modified

--- src/Headers.js
+++ src/Headers.js
@@ -5,9 +5,10 @@ function Headers(props) {
        <div className="col-2 form-group">
           <select className="form-control custom-select">
             {[
-              { n: "Заголовок", t: "div", type: "unstyled***" },
-              { n: "Заголовок 1", t: "h1", type: "header-one" },
-              { n: "Заголовок 2", t: "h2", type: "header-two" }
+              { n: "Заголовок", t: "div" },
+              { n: "Строка", t: "p" },
+              { n: "Заголовок 1", t: "h1" },
+              { n: "Заголовок 2", t: "h2" }
             ].map((font, i) => (
               <option
                 key={i + 33}
