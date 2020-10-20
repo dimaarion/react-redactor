0a3208363b7ed927d22e79e7eea720c8b4ca2694
dimaarion <dimaarion@gmail.com>
2020-10-20 12:04:34 +0300

    1 file modified

--- src/FamelyF.js
+++ src/FamelyF.js
@@ -4,7 +4,7 @@ function FamelyF(props) {
   return (
    <div className="col-8 form-group text-left p-0">
             <select className="col form-control custom-select"  onChange = {(e)=>props.setfontFm(e.target.value)}>
-              {["Georgia", "Roboto", "Arial","Montserrat"].map( font => 
+              {["Georgia", "Roboto", "Arial","Montserrat","Krona One"].map( font => 
                 <option key={font}>{font}</option>
               )}
             </select>
