9f54ba2e658d6bdc9aa6ce87899b7a519f06623f
dimaarion <dimaarion@gmail.com>
2020-10-21 01:33:03 +0300

    1 file modified

--- src/Inputs.js
+++ src/Inputs.js
@@ -2,14 +2,14 @@ import React from "react";
 
 
 function Inputs(props) {
-  let c = ''
+  let c = '';
   if(!props.c){
       c = '';
 }else{
-  c = props.c
+  c = props.c;
 }
   return (
-    <div className={"col-" + props.col + ' text-left ' + props.c }>
+    <div className={"col-" + props.col + ' text-left ' + c }>
      <label htmlFor={props.id}>{props.name}</label>
      <input id = {props.id} className="form-control form-control-lg" type="text" placeholder={props.text} onChange = {(e)=>props.cange(e.target.value)}/>
     </div>
