9b3f09bb452657fbcdd3611469a0e9c921241200
dimaarion <dimaarion@gmail.com>
2020-10-21 02:09:14 +0300

    1 file modified

--- src/ImgRemove.js
+++ src/ImgRemove.js
@@ -9,7 +9,7 @@ function ImgRemove(props) {
     <h2 className = "text-left">Редактирование изображения</h2>
     <div className = "row">
     <div className = "col-3">
-     <Inputs name = 'Ширина' id = "width" col = "sm" cange = {()=>{}} />
+     <Inputs name = 'Ширина' id = "width" col = "sm" c = "text-center" cange = {()=>{}} />
      <Inputs name = 'Высота' id = "height" col = "sm" cange = {()=>{}} />
      </div>
      <div className = "col-2 text-left row mt-4">
@@ -22,7 +22,9 @@ function ImgRemove(props) {
       <div className = "col">
       <Lineright/>
         </div>
+        <Inputs name = 'Отступ' id = "padding" col = "9" c = "mt-4 pt-2" cange = {()=>{}} />
      </div>
+     <div className = "col text-left"><div className = "btn btn-primary mb-2">Сохранить</div></div>
      </div>
     </div>
   );
