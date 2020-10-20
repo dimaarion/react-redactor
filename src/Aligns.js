5df3acd91f7724d47c5507009ba71c845142490e
dimaarion <dimaarion@gmail.com>
2020-10-20 18:08:31 +0300

    2 files modified

--- src/Aligns.js
+++ src/Aligns.js
@@ -1,23 +1,11 @@
 import React from "react";
-
+import LineLeft from "LineLeft";
 function Aligns(props) {
   return (
      <div className="col-2 aligns">
           <div className="row text-left">
-            <div className="col">
-              <svg onClick = {()=>props.setalign('left')}
-                width="1em"
-                height="1em"
-                viewBox="0 0 16 16"
-                className="bi bi-justify-left"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M2 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"
-                />
-              </svg>
+            <div onClick = {()=>props.setalign('left')} className="col" >
+              
             </div>
             <div className="col">
               <svg onClick = {()=>props.setalign('center')}
---
+++ src/LineLeft.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function LineLeft(props) {
+  return (
+    <svg onClick = {()=>props.setalign('left')}>
+                width="1em"
+                height="1em"
+                viewBox="0 0 16 16"
+                className="bi bi-justify-left"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"
+                />
+              </svg>
+  );
+}
+export default LineLeft;
