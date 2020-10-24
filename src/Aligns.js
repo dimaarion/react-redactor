1973735a8005ace3f2168b42a2cd597d8dd6618f
Dmitriy <48031182+dimaarion@users.noreply.github.com>
2020-10-24 06:40:20 +0300

    dimaarion/react-redactor.git

--- src/Aligns.js
+++ src/Aligns.js
@@ -2,22 +2,44 @@ import React, { useState, useEffect } from "react";
 import Lineleft from "./Lineleft";
 import Linecenter from "./Linecenter";
 import Lineright from "./Lineright";
-import './css/line.css';
+import "./css/line.css";
 function Aligns(props) {
-  const [itemsLine, setitemsLine] = useState('no');
+  const [itemsLine, setitemsLine] = useState("no");
 
   useEffect(() => {
     setitemsLine(props.align);
   }, [props.align]);
   return (
-    
-    <div className="row  p-2">
-      <Lineleft itemsLine={itemsLine} setitemsLine={setitemsLine} a ={'left'} align={props.align} aligns={props.aligns} style={{ height: 'auto' }} ititalTegs={props.ititalTegs} items={props.items} />
-      <Linecenter itemsLine={itemsLine} setitemsLine={setitemsLine} a={'center'} aligns={props.aligns} style={{ height: 'auto' }} ititalTegs={props.ititalTegs} items={props.items} />
-      <Lineright itemsLine={itemsLine} setitemsLine={setitemsLine} a={'right'} aligns={props.aligns} style={{ height: 'auto' }} ititalTegs={props.ititalTegs} items={props.items} />
-      </div>
-
-    
+    <div className="row  p-2 aligns">
+      <Lineleft
+        itemsLine={itemsLine}
+        setitemsLine={setitemsLine}
+        a={"left"}
+        align={props.align}
+        aligns={props.aligns}
+        style={{ height: "auto" }}
+        ititalTegs={props.ititalTegs}
+        items={props.items}
+      />
+      <Linecenter
+        itemsLine={itemsLine}
+        setitemsLine={setitemsLine}
+        a={"center"}
+        aligns={props.aligns}
+        style={{ height: "auto" }}
+        ititalTegs={props.ititalTegs}
+        items={props.items}
+      />
+      <Lineright
+        itemsLine={itemsLine}
+        setitemsLine={setitemsLine}
+        a={"right"}
+        aligns={props.aligns}
+        style={{ height: "auto" }}
+        ititalTegs={props.ititalTegs}
+        items={props.items}
+      />
+    </div>
   );
 }
 export default Aligns;
