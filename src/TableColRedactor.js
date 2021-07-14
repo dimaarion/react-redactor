dbf9d002bdafde52ec6da94a43340f039103d988
dimaarion <dimaarion@gmail.com>
2021-07-14 23:47:38 +0300

    dimaarion/react-redactor.git

--- src/TableColRedactor.js
+++ src/TableColRedactor.js
@@ -1,10 +1,20 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import "./css/tablecolredactor.css"
 export default function TableColRedactor(props) {
+     const [col, setCol] = useState(0);
+     const [colr, setColr] = useState(0);
+     useEffect(()=>{
+         if(props.find.tagName === "TD"){
+              props.find.setAttribute("style","width:"+ col +"px");
+              setColr(props.find.getAttribute("style"));
+         }
+       
+     },[col, props.find])
+    
     return (
-    <div className="row ml-4 pl-3">
+    <div className="row ">
         <div className="col-sm">
         </div>
-        <input className="form-control col-sm" type="text" placeholder="0" aria-label="default input example" />
+        <input onChange = {(e)=>setCol(e.target.value)} className="form-control col-sm-1 tablecolredactor" type="text" placeholder="0" aria-label="default input example" />
     </div>) 
 }
