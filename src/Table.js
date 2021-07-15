0aae2b50fe22a04b886a31feb61f6f3a2d73bd94
Dmitriy <48031182+dimaarion@users.noreply.github.com>
2021-07-15 10:28:27 +0300

    dimaarion/react-redactor.git

--- src/Table.js
+++ src/Table.js
@@ -4,6 +4,7 @@ function Table(props) {
   const [col, setCol] = useState(4);
   const [str, setStr] = useState(2);
   const [openTb, setopenTb] = useState(false);
+  const [dSittens, setDSittens] = useState(false);
   const [width, setWidth] = useState("100%");
   const [float, setFloat] = useState("none");
   let arr_en = [
@@ -36,7 +37,25 @@ function Table(props) {
   ];
   let active = { padding: "2px", border: "solid 1px #cccccc" };
   return (
-    <div>
+    <div className="tablesreate">
+      <div className="tsittens">
+        <button
+          onClick={() => setDSittens(true)}
+          type="button"
+          className="tableButton"
+          title="Настройка таблицы"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            fill="currentColor"
+            class="bi bi-wrench"
+            viewBox="0 0 16 21"
+          >
+            <path d="M.102 2.223A3.004 3.004 0 0 0 3.78 5.897l6.341 6.252A3.003 3.003 0 0 0 13 16a3 3 0 1 0-.851-5.878L5.897 3.781A3.004 3.004 0 0 0 2.223.1l2.141 2.142L4 4l-1.757.364L.102 2.223zm13.37 9.019.528.026.287.445.445.287.026.529L15 13l-.242.471-.026.529-.445.287-.287.445-.529.026L13 15l-.471-.242-.529-.026-.287-.445-.445-.287-.026-.529L11 13l.242-.471.026-.529.445-.287.287-.445.529-.026L13 11l.471.242z" />
+          </svg>
+        </button>
+      </div>
       <button type="button" className="tableButton">
         <svg
           onClick={() => setopenTb(true)}
@@ -52,9 +71,13 @@ function Table(props) {
           />
         </svg>
       </button>
-     <div>
-       <TableColRedactor/>
-     </div>
+      <div>
+        {dSittens === true ? (
+          <TableColRedactor setDSittens={setDSittens} />
+        ) : (
+          ""
+        )}
+      </div>
       {openTb === true ? (
         <div className="tableRedactor">
           <table className="table table-hover">
@@ -192,7 +215,6 @@ function Table(props) {
                           }
                         : () => {}
                     }
-                    
                     className="btn btn-primary "
                   >
                     Ок
@@ -203,7 +225,6 @@ function Table(props) {
                       setopenTb(false);
                       props.setActive(props.active === false ? true : false);
                     }}
-                   
                     className="btn btn-primary ml-2"
                   >
                     Отмена
--- src/TableColRedactor.js
+++ src/TableColRedactor.js
@@ -1,20 +1,70 @@
 import React, { useEffect, useState } from "react";
-import "./css/tablecolredactor.css"
+import "./css/tablecolredactor.css";
 export default function TableColRedactor(props) {
-     const [col, setCol] = useState(0);
-     const [colr, setColr] = useState(0);
-     useEffect(()=>{
-         if(props.find.tagName === "TD"){
-              props.find.setAttribute("style","width:"+ col +"px");
-              setColr(props.find.getAttribute("style"));
-         }
-       
-     },[col, props.find])
-    
-    return (
-    <div className="row ">
-        <div className="col-sm">
-        </div>
-        <input onChange = {(e)=>setCol(e.target.value)} className="form-control col-sm-1 tablecolredactor" type="text" placeholder="0" aria-label="default input example" />
-    </div>) 
+  const [col, setCol] = useState(0);
+  const [colr, setColr] = useState(0);
+
+  useEffect(() => {
+    if (props.find !== undefined && props.find.tagName === "TD") {
+      props.find.setAttribute("style", "width:" + col + "px");
+      setColr(props.find.getAttribute("style"));
+    }
+  }, [col, props.find]);
+  const STYLES = {
+    close: {
+      textAlign: "right"
+    },
+    but: {
+      textAlign: "center"
+    }
+  };
+  return (
+    <div className="tableRedactor tablecolredactor">
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <td>Настройки таблицы</td>
+            <td style={STYLES.close}>
+              <svg
+                onClick={() => {
+                  props.setDSittens(false);
+                }}
+                width="2em"
+                viewBox="0 0 16 16"
+                className="bi bi-x"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
+                />
+              </svg>
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Ширина столбца</td>
+            <td>
+              <input
+                onChange={(e) => setCol(e.target.value)}
+                className="form-control "
+                type="text"
+                placeholder="0"
+                aria-label="default input example"
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button type="button" className="btn btn-primary">
+                Ок
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
